29dea80ead26d4b2357ba89769c6388a366ab0f3
Vincent and Niranjan <nrbramadas@gmail.com>
2013-01-16 10:36:24 -0800

    Added empty classes for placeholders

---
+++ chess.rb
@@ -0,0 +1,35 @@
+class Game
+
+end
+
+class GameBoard
+
+end
+
+class Piece
+
+end
+
+class Pawn < Piece
+
+end
+
+class Rook < Piece
+
+end
+
+class Bishop < Piece
+
+end
+
+class Knight < Piece
+
+end
+
+class King < Piece
+
+end
+
+class Queen < Piece
+
+end
