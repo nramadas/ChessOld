43205f687c78b9b083519ceb7f02948086b3525d
Vincent and Niranjan <nrbramadas@gmail.com>
2013-01-16 10:52:35 -0800

    Added gameboard initialize

--- chess.rb
+++ chess.rb
@@ -4,9 +4,22 @@ end
 
 class GameBoard
 
+	def initialize
+		@board = Array.new(8) { Array.new(8) { nil } }
+	end
+
+	def place_pawns
+
+	end
+
+
 end
 
 class Piece
+	def initialize(coordinates)
+		@coordinates = coordinates
+		@captured = false
+	end
 
 end
 
